09be8fea1ecdf4939b9bc24f000d7567dfe0a922
xiaofei20190625 <981962598@qq.com>
2019-06-25 23:37:24 +0800

    commit by idea

---
+++ demo1/src/main/java/com/xiaofei/bean/Student.java
@@ -0,0 +1,13 @@
+package com.xiaofei.bean;
+
+public class Student {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
